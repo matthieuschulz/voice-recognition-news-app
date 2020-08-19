ca5b5d866fa530a348f5d344a52aa7382c21fa50
Matthieu Schulz <m.schulz18@ejm.org>
2020-08-19 15:41:30 +0200

    update

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+const alanKey = '293605cf81593945a63559dc10b162c12e956eca572e1d8b807a3e2338fdd0dc/stage';
+const App = () => {
+
+    useEffect(() => {
+        alanBtn({ 
+            key: alanKey,
+        onCommand: ( {command}) => {
+            if(command === 'testCommand') {
+                alert('this code was executed');
+            }
+        }
+    })
+    }, [])
+
+    return (
+        <div>
+           <h1>Alan AI News Application</h1> 
+        </div>
+    );
+}
+
+export default App;
