4d3565d7b4f1b24b162add1bf653e0ab2b6db77e
Matthieu Schulz <m.schulz18@ejm.org>
2020-08-30 02:16:08 +0200

    update

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import alanBtn from '@alan-ai/alan-sdk-web';
 const alanKey = '293605cf81593945a63559dc10b162c12e956eca572e1d8b807a3e2338fdd0dc/stage';
 const App = () => {
 
-    
+
 
     useEffect(() => {
         alanBtn({ 
 
             key: alanKey,
 
+            
         onCommand: ( {command, articles}) => {
             if(command === 'newHeadlines') {
                 console.log(articles);
