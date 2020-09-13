077f626be888c4a85e54601967bde6f34e0fa796
Matthieu Schulz <m.schulz18@ejm.org>
2020-09-13 20:16:36 +0100

    Updates

---
+++ precache-manifest.9f06b274bea0ba75eb7e4476618dc500.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c84c0ec9e04be6993416a0d4cd30b6e4",
+    "url": "/voice-recognition-news-app/index.html"
+  },
+  {
+    "revision": "f5826b7fda2bdda552e4",
+    "url": "/voice-recognition-news-app/static/css/main.041c3db4.chunk.css"
+  },
+  {
+    "revision": "29b83e3f5dd3427eeff4",
+    "url": "/voice-recognition-news-app/static/js/2.a8b0103f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/voice-recognition-news-app/static/js/2.a8b0103f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5826b7fda2bdda552e4",
+    "url": "/voice-recognition-news-app/static/js/main.7d6f9d3e.chunk.js"
+  },
+  {
+    "revision": "a23f7c0375c5be7add6a",
+    "url": "/voice-recognition-news-app/static/js/runtime-main.6512503a.js"
+  }
+]);
