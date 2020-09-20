c5bb7305888b950b936045487e2239f77e973f88
Matthieu Schulz <m.schulz18@ejm.org>
2020-09-20 19:09:30 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React, { useEffect } from 'react';
-import alanBtn from '@alan-ai/alan-sdk-web';
+import React, { useEffect } from 'react'; 
+import alanBtn from '@alan-ai/alan-sdk-web'; // importing the alan btn from the dependency that we just installed
 
 const alanKey = '293605cf81593945a63559dc10b162c12e956eca572e1d8b807a3e2338fdd0dc/stage';
 const App = () => {
 
-
-
-    useEffect(() => {
+    useEffect(() => {// now initialise the alan bottun, use effect is a function once our application starts
         alanBtn({ 
 
-            key: alanKey,
+            key: alanKey, // key that allows us to use alan
 
         onCommand: ( {command, articles}) => {
             if(command === 'newHeadlines') {
                 console.log(articles);
             }
-
-
-
-
-
-
-
-
 
 
         }
