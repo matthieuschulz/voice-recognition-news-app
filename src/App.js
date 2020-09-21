00ccc19defa91d463811d7294248d8b1794c5348
Matthieu Schulz <m.schulz18@ejm.org>
2020-09-21 01:58:55 +0100

    update

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import alanBtn from '@alan-ai/alan-sdk-web'; // importing the alan btn from the 
 const alanKey = '293605cf81593945a63559dc10b162c12e956eca572e1d8b807a3e2338fdd0dc/stage';
 const App = () => {
 
-    useEffect(() => {// now initialise the alan bottun, use effect is a function once our application starts
+    useEffect(() => {// now initialise the alan bottn, use effect is a function once our application starts
         alanBtn({ 
 
             key: alanKey, // key that allows us to use alan
 
-        onCommand: ( {command, articles}) => {
+        onCommand: ( {command, articles}) => { // when alan heres this command...,
             if(command === 'newHeadlines') {
                 console.log(articles);
             }
         }
-        
+
     })
 
     }, [])
