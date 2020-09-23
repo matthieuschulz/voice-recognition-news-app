9f6313a9a5b67b850599b142ce5550ccdfcb9119
Matthieu Schulz <m.schulz18@ejm.org>
2020-09-24 00:58:44 +0100

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import alanBtn from '@alan-ai/alan-sdk-web'; // importing the alan btn from the 
 const alanKey = '293605cf81593945a63559dc10b162c12e956eca572e1d8b807a3e2338fdd0dc/stage';
 const App = () => {
 
-    useEffect(() => {// now initialise the alan bottn, use effect is a function once our application starts
+    useEffect(() => { // now initialise the alan bottn, use effect is a function once our application starts
+    // first paratmeter is a callback function
         alanBtn({ 
 
             key: alanKey, // key that allows us to use alan
@@ -14,14 +15,15 @@ const App = () => {
                 console.log(articles);
             }
         }
-        
 
+
+        
     })
 
-    }, [])
+    }, [])//second parameter is a dependency array)
     return (
         <div>
-           <h1>Alan AI News Application</h1> 
+           <h1>Voice Recognition New App using AlanAI</h1> 
         </div>
     );    
 }
