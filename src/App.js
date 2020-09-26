f7bd3865ffb9128c10a0464d33138ce9e192cdb4
Matthieu Schulz <m.schulz18@ejm.org>
2020-09-26 17:13:54 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { useEffect } from 'react'; 
+import React, { useState, useEffect } from 'react'; 
 import alanBtn from '@alan-ai/alan-sdk-web'; // importing the alan btn from the dependency that we just installed
+import NewsCards from './Components/NewsCards/NewsCards';
 
 const alanKey = '293605cf81593945a63559dc10b162c12e956eca572e1d8b807a3e2338fdd0dc/stage';
 const App = () => {
+    const [newsArticles, setNewsArticles] = useState([]); // goal is to set articles to the state, to then display them in newscards
 
     useEffect(() => { // now initialise the alan bottn, use effect is a function once our application starts
     // first paratmeter is a callback function
@@ -12,7 +14,7 @@ const App = () => {
 
         onCommand: ( {command, articles}) => { // when alan heres this command...,
             if(command === 'newHeadlines') {
-                console.log(articles);
+                setNewsArticles(articles);
             }
         }
 
@@ -25,6 +27,7 @@ const App = () => {
     return (
         <div>
            <h1>Voice Recognition New App using AlanAI</h1> 
+           <NewsCards articles = {newsArticles}/>
         </div>
     );    
 }
