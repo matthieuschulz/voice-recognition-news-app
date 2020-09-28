c70e3542771cf0e61e786cc2f7fddf14dca3b4d6
Matthieu Schulz <m.schulz18@ejm.org>
2020-09-29 00:47:44 +0100

    update

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ const App = () => {
 
             
 
+
         onCommand: ( {command, articles}) => { // when alan heres this command...,
             if(command === 'newHeadlines') {
                 setNewsArticles(articles);
