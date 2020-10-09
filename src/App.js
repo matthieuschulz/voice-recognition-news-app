ab509109791dca270bcebea1dc0df27c9e9f3410
Matthieu Schulz <m.schulz18@ejm.org>
2020-10-09 23:35:37 +0100

    update

--- src/App.js
+++ src/App.js
@@ -15,9 +15,10 @@ const App = () => {
             key: alanKey, // key that allows us to use alan
 
         onCommand: ( {command, articles}) => { // when alan heres this command...,
+            
             if(command === 'newHeadlines') {
                 setNewsArticles(articles);
-                
+
 
             }
 
