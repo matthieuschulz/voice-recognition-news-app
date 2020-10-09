5e4125287be6ac98a29c5ebb6807b527197d61b8
Matthieu Schulz <m.schulz18@ejm.org>
2020-10-09 23:36:21 +0100

    update

--- src/App.js
+++ src/App.js
@@ -15,22 +15,13 @@ const App = () => {
             key: alanKey, // key that allows us to use alan
 
         onCommand: ( {command, articles}) => { // when alan heres this command...,
-            
+
             if(command === 'newHeadlines') {
                 setNewsArticles(articles);
-
-
             }
-
         }
 
-
         
-
-
-
-
-
     })
 
     }, [])//second parameter is a dependency array)
